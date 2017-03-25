6e047cf31c592267d79b679cf3122b292b42728a
Carl Custav Mäeorg <carlmaeorg@hotmail.com>
2017-03-25 15:11:51 +0200

    some changes

---
+++ K3/WildAnimalsRegister/static/angular/script.js
@@ -0,0 +1,40 @@
+/** Created by carlcustav on 3/21/2017. */
+var app = angular.module('wildAnimals', ['ngRoute']);
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/search", {
+            templateUrl: "templates/lisamine.html",
+            controller: 'DogsController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
+app.controller('wildAnimalsController', function ($scope, $http) {
+
+    $scope.searchResult = [];
+    $scope.queryType = '';
+
+    $scope.searchByName = function(event) {
+        event.preventDefault();
+        var form = $('#FormSearchByName');
+        $http({
+            method: "POST",
+            url: 'searchByName/',
+            data: $.param(form.serializeArray()),
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).then(
+            function success(response) { $scope.searchResult = response.data; },
+            function error(response) { alert(response); })
+    }
+
+    //getters & setters
+     $scope.setQueryType = function (queryType) {
+        $scope.queryType = queryType;
+    }
+    $scope.getQueryType = function () {
+        return $scope.queryType;
+    }
+
+
+});
---
+++ K3/WildAnimalsRegister/urls.py
@@ -0,0 +1,8 @@
+__author__ = 'carlcustav'
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^searchByName/', views.searchByName, name='searchByName')
+]
